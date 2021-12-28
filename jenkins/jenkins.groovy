4c773a47d8a6e62819123b56d4b02f6036bcc7b2
Dinesh Kumaran <d,kumaran.m@gmail.com>
2021-12-28 20:12:08 +0530

    added jenkins file

---
+++ jenkins/jenkins.groovy
@@ -0,0 +1,63 @@
+pipeline{
+    agent any
+    tools{
+        maven 'Maven3.8.4'
+    }
+    stages{
+        stage("Execute shell"){
+            steps{
+                sh '''echo "This is my first Job"
+                echo $JOB_NAME'''
+            }
+            post{
+                success{
+                    echo "shell successfully completed=="
+                }
+                failure{
+                    echo "shell failed"
+                }
+            }
+        }
+        stage ("code checkout"){
+            steps{
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/DineshKumaran91/day_01.git']]])
+            }
+            post{
+                success{
+                    echo "successfully cloned from Repo"
+                }
+                failure{
+                    echo "failed to clone"
+                }
+            }
+        }
+        stage('packaging'){
+            steps{
+                sh 'mvn -B -DskipTests clean package'
+            }
+            post{
+                success{
+                    echo "build completed"
+                    archive 'target/*.jar'
+                }
+                failure{
+                    echo "build failed"
+                }
+            }
+        }
+        stage('Testing'){
+            steps{
+                sh 'mvn test'
+            }
+            post{
+                success{
+                    echo "test completed"
+                    junit "target/surefire-reports/*.xml"
+                }
+                failure{
+                    echo "test failed"
+                }
+            }
+        }
+    }
+}
