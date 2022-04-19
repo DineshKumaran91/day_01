7131e86d70431a67a804ac80dbff5b15289d8065
DineshKumaran91 <85617607+DineshKumaran91@users.noreply.github.com>
2022-04-19 13:26:42 +0530

    update
    update

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,62 +1,13 @@
 pipeline{
-    agent any
-    tools{
-        maven 'Maven3.8.4'
-    }
+    agent none
     stages{
-        stage("Execute shell"){
+        stage("Test"){
+            agent any
+            options {
+                skipDefaultCheckout()
+            }
             steps{
-                sh '''echo "This is my first Job"
-                echo $JOB_NAME'''
-            }
-            post{
-                success{
-                    echo "shell successfully completed=="
-                }
-                failure{
-                    echo "shell failed"
-                }
-            }
-        }
-        stage ("code checkout"){
-            steps{
-                checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/DineshKumaran91/day_01.git']]])
-            }
-            post{
-                success{
-                    echo "successfully cloned from Repo"
-                }
-                failure{
-                    echo "failed to clone"
-                }
-            }
-        }
-        stage('packaging'){
-            steps{
-                sh 'mvn -B -DskipTests clean package'
-            }
-            post{
-                success{
-                    echo "build completed"
-                    archive 'target/*.jar'
-                }
-                failure{
-                    echo "build failed"
-                }
-            }
-        }
-        stage('Testing'){
-            steps{
-                sh 'mvn test'
-            }
-            post{
-                success{
-                    echo "test completed"
-                    junit "target/surefire-reports/*.xml"
-                }
-                failure{
-                    echo "test failed"
-                }
+                echo "Hello world"
             }
         }
     }
